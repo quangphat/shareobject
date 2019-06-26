1810fbea28cae4afdec65cab54d0e82eb17a9030
quangphat <phat.dinhquang@haravan.com>
2019-06-26 14:26:29 +0700

    init

---
+++ my8.ShareObject/CommentMsg.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace my8.ShareObject.ESB
+{
+    public class CommentMsg
+    {
+        public int Id { get; set; }
+        public string Comment { get; set; }
+    }
+}
