0e4b95c10170988ac9dcf96a375a8e4716de0bbf
quangphat <phat.dinhquang@haravan.com>
2019-07-21 00:08:33 +0700

    RecommendedTagMsg.cs

---
+++ my8.ShareObject/RecommendedTagMsg.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace my8.ShareObject.ESB
+{
+    public class RecommendedTagMsg
+    {
+        public List<string> Tag { get; set; }
+    }
+}
