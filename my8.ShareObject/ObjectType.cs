5f6bba9e218ac1e8c19623fde1741c88e0bd84c9
quangphat <phat.dinhquang@haravan.com>
2019-07-23 00:10:31 +0700

    searchmodel

---
+++ my8.ShareObject/ObjectType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace my8.ShareObject.ESB
+{
+    public enum ObjectType
+    {
+        Article=1,
+        Job=2,
+        People = 3,
+        Company = 4
+    }
+}
---
+++ my8.ShareObject/SearchModelMsg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace my8.ShareObject.ESB
+{
+    public class SearchModelMsg
+    {
+        public string Id { get; set; }
+        public string FeedId { get; set; }
+        public ObjectType ObjectType { get; set; } 
+        public string Title { get; set; }
+        public string[] Tags { get; set; } 
+        public string ProjectId { get; set; }
+    }
+}
