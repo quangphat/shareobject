a923b5c1d21f4ba2bdf47b2e03e555aa7fe3a338
quangphat <phat.dinhquang@haravan.com>
2020-05-01 00:16:58 +0700

    init CoreObject

---
+++ CoreObject/Mongo/MGPagination.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreObject.Mongo
+{
+    [BsonIgnoreExtraElements]
+    public class MGPagination<TEntity>
+    {
+        public int total { get; set; }
+        public IEnumerable<TEntity> datas { get; set; }
+    }
+}
---
+++ CoreObject/Pagination.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreObject
+{
+    public class Pagination<T> where T : class
+    {
+        public List<T> Datas { get; set; }
+        public int TotalRecord { get; set; }
+    }
+}
