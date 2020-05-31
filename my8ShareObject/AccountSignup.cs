98e85b6e8e9c86dffef3777e3a233a36d74a7f14
quangphat <phat.dinhquang@haravan.com>
2020-05-31 14:57:27 +0700

    add Account, AccountLogin, AccountSignup

---
+++ my8ShareObject/AccountSignup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace my8ShareObject
+{
+    public class AccountSignup
+    {
+        public string DisplayName { get; set; }
+        public string ProfileName { get; set; }
+        public string Email { get; set; }
+        public string[] SkillTags { get; set; }
+        public string Password { get; set; }
+    }
+}
