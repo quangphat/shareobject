cbb77f398c5aa4e1e8099bdde614c0915db24921
quangphat <phat.dinhquang@haravan.com>
2020-11-15 23:52:49 +0700

    AmazonS3Config

--- my8ShareObject/AmazonS3Config.cs
+++ my8ShareObject/AmazonS3Config.cs
@@ -1,9 +1,10 @@
 ﻿
-    public class AmazonS3Config
-    {
-        public string Host { get; set; }
-        public string BucketName { get; set; }
-        public string AccessKeyId { get; set; }
-        public string AccessKey { get; set; }
-    }
+public class AmazonS3Config
+{
+    public string Host { get; set; }
+    public string BucketName { get; set; }
+    public string AccessKeyId { get; set; }
+    public string AccessKey { get; set; }
+    public string UserProfileCv { get; set; }
+}
 
