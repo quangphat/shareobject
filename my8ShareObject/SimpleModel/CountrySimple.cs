628c30895939aaf40412c694311c9efe6d983b00
quangphat <phat.dinhquang@haravan.com>
2021-07-11 00:27:02 +0700

    country

---
+++ my8ShareObject/SimpleModel/CountrySimple.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace my8ShareObject.SimpleModel
+{
+    public class CountrySimple
+    {
+        public string Id { get; set; }
+        public string Code { get; set; }
+        public string CountryName { get; set; }
+        public string FlagUrl { get; set; }
+    }
+}
